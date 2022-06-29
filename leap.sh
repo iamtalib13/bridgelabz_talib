359af03b288c8b6698c53a3d7f045f9ca0b21458
talib <talibsheikh16@gmail.com>
2022-06-29 12:21:34 +0530

    Leap year shell script using if else

--- leap.sh
+++ leap.sh
@@ -1,11 +1,10 @@
-clear
-echo "LEAP YEAR SHELL SCRIPT"
+echo "LEAP YEAR"
 echo -n "Enter a year:"
-read year_checker
-if [ `expr $year_checker % 4` -eq 0 ]
+read year
+if [ `expr $year % 4` -eq 0 ]
 then
-	echo "$year_checker is a leap year"
+	echo "$year is a leap year"
 else
-	echo "$year_checker is not a leap year"
+	echo "$year is not a leap year"
 fi
 
