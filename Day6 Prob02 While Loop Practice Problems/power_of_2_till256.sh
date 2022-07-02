e429b6b517a5535853fea10a98a357317810bf92
iamtalib13 <talibsheikh16@gmail.com>
2022-07-02 16:57:09 +0530

    Added Repetition practice problems with while loop

---
+++ Day6 Prob02 While Loop Practice Problems/power_of_2_till256.sh
@@ -0,0 +1,16 @@
+
+echo "Enter Number"
+read num 
+power=1
+count=0
+#counting till power reached
+while [ $count -le $num ]
+do
+	#checking if less than 256
+	if [ $power -le 256 ]
+	then
+		echo $power
+		power=$(($power*2))
+	fi
+	((count++))
+done
